0c3fe223de393700da8f09b9cb2c1ee5dab8ff5a
throuz <zed2786328@gmail.com>
2021-07-19 20:00:07 +0800

    Organize home related components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,11 +9,11 @@
 </template>
 
 <script>
-import HomeBanner from "@/components/HomeBanner.vue";
-import HomeNews from "@/components/HomeNews.vue";
-import HomeReason from "@/components/HomeReason.vue";
-import HomeCollection from "@/components/HomeCollection.vue";
-import HomePurchase from "@/components/HomePurchase.vue";
+import HomeBanner from "@/components/Home/HomeBanner.vue";
+import HomeNews from "@/components/Home/HomeNews.vue";
+import HomeReason from "@/components/Home/HomeReason.vue";
+import HomeCollection from "@/components/Home/HomeCollection.vue";
+import HomePurchase from "@/components/Home/HomePurchase.vue";
 
 export default {
   name: "Home",
