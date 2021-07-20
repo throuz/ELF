cb39c3c7fd4fe65d5e1a36fa999df760897a5522
throuz <zed2786328@gmail.com>
2021-07-20 22:59:58 +0800

    Complete Consolidation Banner

---
+++ src/components/Consolidation/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="banner">集貨介紹</div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+.banner {
+  background: #808695;
+  color: #f8f8f9;
+  line-height: 15vh;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
---
+++ src/views/Consolidation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="consolidation">
+    <Banner />
+  </div>
+</template>
+
+<script>
+import Banner from "@/components/Consolidation/Banner.vue";
+
+export default {
+  name: "Consolidation",
+  components: {
+    Banner,
+  },
+};
+</script>
